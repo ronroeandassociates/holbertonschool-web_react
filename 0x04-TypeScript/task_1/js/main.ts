984ae4d0084db60ecffa8a337658910821527ac5
Ronroeandassociates <3321@holbertonschool.com>
2022-02-14 18:26:40 -0600

    4. Writing a class mandatory Write a Class named StudentClass:
    The constructor accepts firstName(string) and lastName(string) arguments
    The class has a method named workOnHomework that return the string Currently working
    The class has a method named displayName. It returns the firstName of the student
    The constructor of the class should be described through an Interface
    The class should be described through an Interface
    Requirements:
    
    You can reuse the Webpack configuration from the previous exercise to compile the code.
    When running npm run build, no TypeScript error should be displayed.
    Every variable should use TypeScript when possible.

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -27,3 +27,37 @@ export let printTeacher: printTeacherFunction
 printTeacher = function (firstName: string, lastName: string): string {
   return `${firstName[0]}. ${lastName}`
 }
+
+/* setup interface studentclass */
+
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentClassInterface
+}
+
+interface StudentClassInterface {
+  firstName: string
+  lastName: string
+  displayName(): string
+  workOnHomework(): string
+}
+/* output some information */
+
+export const StudentClass: StudentConstructor = class StudentClass
+  implements StudentClassInterface
+{
+  firstName: string
+  lastName: string
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName
+    this.lastName = lastName
+  }
+
+  workOnHomework() {
+    return "Currently working"
+  }
+
+  displayName() {
+    return this.firstName
+  }
+}
