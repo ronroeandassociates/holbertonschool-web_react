085cde5f9c773e602878f34bfb6129573f123606
Ronroeandassociates <3321@holbertonschool.com>
2022-09-28 22:32:07 -0500

    probably should update more often

---
+++ 0x01-react_intro/task_3/dashboard/src/utils.test.js
@@ -0,0 +1,28 @@
+import { getFullYear, getFooterCopy, getLatestNotification } from './utils'
+
+
+// test to check getFullYear returns correct year
+test('getFullYear returns correct year', () => {
+	  expect(getFullYear()).toBe(2022);
+});
+
+
+// test to check that getFooter returns correct string
+// when arg is false
+test('getFooter returns correct string if false', () => {
+	  expect(getFooterCopy(false)).toBe('Holberton School main dashboard');
+});
+
+// test to check that getFooter returns correct string
+// when arg is true
+test('getFooter returns correct string if true', () => {
+	expect(getFooterCopy(true)).toBe('Holberton School');
+});
+
+
+
+// test to check that getLatestNotification returns correct
+// html string
+test('getLatestNotification returns correct html string', () => {
+	  expect(getLatestNotification().__html).toBe('<strong>Urgent requirement</strong> - complete by EOD');
+});
