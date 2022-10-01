8daf82f4372a83c5275f1eae95a64d48559095f5
Ronroeandassociates <3321@holbertonschool.com>
2022-09-30 23:14:23 -0500

    fix 2 items

--- 0x01-react_intro/task_5/dashboard/src/index.js
+++ 0x01-react_intro/task_5/dashboard/src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './App/App.css';
 import App from './App/App';
 import Notifications from './Notifications/Notifications';
 
 ReactDOM.render(
   <React.StrictMode>
-	<div className="root-notifications">
-	  <Notifications />
-	</div>
-    <App />
+  <div id='root-notifications'>
+    <Notifications />
+  </div>
+  <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
