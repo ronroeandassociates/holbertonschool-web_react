0cd7bfefcad9ad7fdaf5f75bbece46bfb2609e82
Ronroeandassociates <3321@holbertonschool.com>
2022-09-30 20:32:00 -0500

    uploading task_2

--- 0x01-react_intro/task_2/dashboard/src/App.js
+++ 0x01-react_intro/task_2/dashboard/src/App.js
@@ -12,6 +12,17 @@ export default class App extends Component {
 	  			</header>
 	 			<body className="App-body">
 		 		 	<p>Login to access the full dashboard</p>
+					<label htmlFor="email" onClick={() => {
+						// select corresponding input
+						document.getElementById('password').focus();
+					}}>Email</label>
+					<input type="email" id="email" />
+					<label htmlFor="password" onClick={() => {
+						// select corresponding input
+						document.getElementById('password').focus();
+					}}>Password</label>
+					<input type="password" id="password" />
+					<button>OK</button>
 	  			</body>
 				<footer className="App-footer">
 					<p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
