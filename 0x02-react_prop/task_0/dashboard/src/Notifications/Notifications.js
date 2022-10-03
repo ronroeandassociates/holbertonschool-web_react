2905d2bbad79f3160dc8ad8841fca3b6af2a5c8c
Ronroeandassociates <3321@holbertonschool.com>
2022-10-03 11:04:32 -0500

    task 2 complete and fix 1 error

--- 0x02-react_prop/task_0/dashboard/src/Notifications/Notifications.js
+++ 0x02-react_prop/task_0/dashboard/src/Notifications/Notifications.js
@@ -27,4 +27,4 @@ export default function Notification(props) {
 	  </ul>
 	</div>
   )
-}g
+}
--- 0x02-react_prop/task_3/dashboard/src/setupTests.js
+++
@@ -1,4 +0,0 @@
-import Enzyme from 'enzyme';
-import Adapter from '@wojtekmaj/enzyme-adapter-react-16';
-
-Enzyme.configure({ adapter: new Adapter() });
