41f05e91a8964c827f8c5a992430e1a62f79a228
Ronroeandassociates <3321@holbertonschool.com>
2022-10-15 19:38:17 -0500

    task 1 is done

--- 0x00-react_state/task_1/dashboard/src/Login/Login.js
+++ 0x00-react_state/task_1/dashboard/src/Login/Login.js
@@ -2,34 +2,104 @@ import React, { Component } from 'react'
 import { StyleSheet, css } from 'aphrodite'
 import WithLoggingHOC from '../HOC/WithLogging'
 
+
 class Login extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			isLoggedIn: false,
+            email: '',
+            password: '',
+            enableSubmit: false,
+		}
+	}
+
+    handleLoginSubmit() {
+        this.setState({isLoggedIn: true});
+    }
+
+    handleChangeEmail(event) {
+        this.setState({email: event.target.value});
+    }
+
+    handleChangePassword(event) {
+        this.setState({password: event.target.value});
+    }
+
+    handleChangeEnableSubmit(event) {
+        this.setState({enableSubmit: event.target.value});
+    }
+
 	render() {
 		return (
 			<React.Fragment>
 				<div className="App">
 					<main className={css(loginStyles.appBody)}>
 						<p>Login to access the full dashboard</p>
-						<div className={css(loginStyles.inputs)}>
-							<label className={css(loginStyles.label)} htmlFor="email" onClick={() => {
-								// select corresponding input
-								document.getElementById('password').focus();
-							}}>Email</label>
-							<input type="email" id="email" className={css(loginStyles.input)} />
-							<label className={css(loginStyles.label)} htmlFor="password" onClick={() => {
-								// select corresponding input
-								document.getElementById('password').focus();
-							}}>Password</label>
-							<input type="password" id="password" className={css(loginStyles.input)} />
-							<button className={css(loginStyles.button)}>OK</button>
-						</div>
+						<form>
+							<div className={css(loginStyles.inputs)}>
+								<label
+                                    className={css(loginStyles.label)}
+                                    htmlFor="email"
+                                    onClick={() => {
+									    document.getElementById('password').focus();
+								    }}>
+                                    Email
+                                </label>
+								<input
+                                    type="email"
+                                    id="email"
+                                    className={css(loginStyles.input)}
+                                    value={this.state.email}
+                                    onChange={(event) => {
+                                        if (event.target.value.length > 0 && this.state.password.length > 0) {
+                                            this.setState({enableSubmit: true});
+                                        } else {
+                                            this.setState({enableSubmit: false});
+                                        }
+                                        this.handleChangeEmail(event);
+                                    }}
+                                />
+								<label
+                                    className={css(loginStyles.label)}
+                                    htmlFor="password"
+                                    onClick={() => {
+									    document.getElementById('password').focus();
+								    }}>
+                                    Password
+                                </label>
+								<input
+                                    type="password"
+                                    id="password"
+                                    className={css(loginStyles.input)}
+                                    value={this.state.password}
+                                    onChange={(event) => {
+                                        if (event.target.value.length > 0 && this.state.email.length > 0) {
+                                            this.setState({enableSubmit: true});
+                                        } else {
+                                            this.setState({enableSubmit: false});
+                                        }
+                                        this.handleChangePassword(event);
+                                    }}
+                                />
+								<input
+                                    type="submit"
+									id="submit"
+                                    className={css(loginStyles.button)}
+                                    value="Login"
+                                    disabled={!this.state.enableSubmit}
+                                    onClick={() => {
+                                        this.handleLoginSubmit();
+                                    }}
+                                />
+							</div>
+						</form>
 					</main>
 				</div>
 			</React.Fragment>
 		)
 	}
 }
-
-const primaryColor = '#E11D3F';
 
 const loginStyles = StyleSheet.create({
 	appBody: {
