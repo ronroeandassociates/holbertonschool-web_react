a236a95804b1af7acb5ef8f81b03f12cd09222c1
Ronroeandassociates <3321@holbertonschool.com>
2022-10-18 22:16:20 -0500

    lint working now

--- 0x01-React_Immutable/3-list.js
+++ 0x01-React_Immutable/3-list.js
@@ -1,9 +1,9 @@
 import Immutable from 'immutable';
 
 export function getListObject(array) {
-	return Immutable.List(array);
+  return Immutable.List(array);
 }
 
 export function addElementToList(list, element) {
-	return list.push(element);
+  return list.push(element);
 }
