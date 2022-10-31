d0c92b5b988c63f2da49df714d5a7a7f8455570c
Ronroeandassociates <3321@holbertonschool.com>
2022-10-31 12:41:50 -0500

    made it immutable

--- 0x03-react_redux_reducer_selector/task_1/dashboard/src/reducers/uiReducer.js
+++ 0x03-react_redux_reducer_selector/task_1/dashboard/src/reducers/uiReducer.js
@@ -1,52 +1,42 @@
 import {
-    LOGIN,
-    LOGIN_SUCCESS,
-    LOGIN_FAILURE,
-    LOGOUT,
-    DISPLAY_NOTIFICATION_DRAWER,
-    HIDE_NOTIFICATION_DRAWER
-  } from '../actions/uiActionTypes'
+  LOGIN,
+  LOGIN_SUCCESS,
+  LOGIN_FAILURE,
+  LOGOUT,
+  DISPLAY_NOTIFICATION_DRAWER,
+  HIDE_NOTIFICATION_DRAWER
+} from '../actions/uiActionTypes'
+import { Map } from 'immutable';
 
-  // Initial state for the UI reducer
-  export const initialState = {
-    isNotificationDrawerVisible: false,
-    isUserLoggedIn: false,
-    user: {}
+export const initialState = {
+  isNotificationDrawerVisible: false,
+  isUserLoggedIn: false,
+  user: {}
+}
+
+const uiReducer = (state = Map(initialState), action) => {
+  switch (action.type) {
+    case DISPLAY_NOTIFICATION_DRAWER:
+      return state.set('isNotificationDrawerVisible', true)
+    case HIDE_NOTIFICATION_DRAWER:
+      return state.set('isNotificationDrawerVisible', false)
+    case LOGIN_SUCCESS:
+      return state.merge({
+        isUserLoggedIn: true,
+        user: action.payload
+      })
+    case LOGIN_FAILURE:
+      return state.merge({
+        isUserLoggedIn: false,
+        error: action.payload
+      })
+    case LOGOUT:
+      return state.merge({
+        isUserLoggedIn: false,
+      })
+    default:
+      return state
   }
+}
 
-  // Reducer for the UI
-  const uiReducer = (state = initialState, action) => {
-    switch (action.type) {
-      case DISPLAY_NOTIFICATION_DRAWER:
-        return {
-          ...state,
-          isNotificationDrawerVisible: true
-        }
-      case HIDE_NOTIFICATION_DRAWER:
-        return {
-          ...state,
-          isNotificationDrawerVisible: false
-        }
-      case LOGIN_SUCCESS:
-        return {
-          ...state,
-          isUserLoggedIn: true,
-          user: action.payload
-        }
-      case LOGIN_FAILURE:
-        return {
-          ...state,
-          isUserLoggedIn: false,
-          error: action.payload
-        }
-      case LOGOUT:
-        return {
-          ...state,
-          isUserLoggedIn: false,
-        }
-      default:
-        return state
-    }
-  }
-
-  export default uiReducer
+export default uiReducer
